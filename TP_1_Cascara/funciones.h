1642c5580a3dcfae50dfc7cd7960377492e91df9
aubelelautaro <36264982+aubelelautaro@users.noreply.github.com>
2018-04-12 23:09:59 -0300

    funciones.h

--- TP_1_Cascara/funciones.h
+++ TP_1_Cascara/funciones.h
@@ -1,11 +1,16 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
-/** \brief
- *
- * \param
- * \param
- * \return
- *
+/** int Pide un numero entero
+ *  Operaciones:
+ *  int Suma los numeros que se ingresen
+ *  int Resta los numeros 
+ *  int Multiplica los numeros
+ *  float Divide los numeros
+ *  long Factorial del numero ingresado (A)
+ *  void Muestra las 5 operaciones de los numeros
+ *  int Valida el rango de las opciones que se vayan a ejecutar
+ *  int Valida que el divisor no sea 0
+ *  int Valida que el numero no sea menor a 0
  */
 
 int pedirNumero(char[],int);
